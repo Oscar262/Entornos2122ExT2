bd9a68aa61ead9bb88bd1af735d6df1b8f049d8d
Oscar Sánchez Navas <oscar.sancheznavas@iesfm.onmicrosoft.com>
2022-03-04 09:16:15 +0100

    Problema 1 actualizacion

--- src/Problema1.java
+++ src/Problema1.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Problema1 {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Problema1 {
+
+    public static String cadena_1_20() {
+        String cadena = "1";
+        for (int i = 2; i <= 20; i++) {
+            cadena += i;
+        }
+        return cadena;
+    }
+
+    public static String cadena_1_20_con_Fizz_Buzz() {
+        String cadena = "1";
+        for (int i = 2; i <= 20; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                cadena += "FizzBuzz";
+            } else if (i % 3 == 0) {
+                cadena += "Fizz";
+            } else if (i % 5 == 0) {
+                cadena += "Buzz";
+            } else {
+                cadena += i;
+            }
+        }
+        return cadena;
+    }
 }
--- test/TestsProblema1.java
+++ test/TestsProblema1.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class TestsProblema1 {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class TestsProblema1 {
+
+    @Test
+    public void cadena_1_20_Test(){
+        String cadena=Problema1.cadena_1_20();
+        Assertions.assertEquals("1234567891011121314151617181920", cadena);
+    }
+
+    @Test
+    public void cadena_1_20_con_Fizz_Buzz(){
+        String cadena=Problema1.cadena_1_20_con_Fizz_Buzz();
+        Assertions.assertEquals("12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19Buzz", cadena);
+    }
+
 }
